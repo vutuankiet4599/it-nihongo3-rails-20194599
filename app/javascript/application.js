b1f8b43abbf202d9f59013e08ddf33d61109f2fe
vutuankiet4599 <vutuankiet4599@gmail.com>
2023-01-31 13:39:28 +0700

    bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,10 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import 'bootstrap'
+import { autosize } from 'autosize'
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="popover"]').popover()
+})
