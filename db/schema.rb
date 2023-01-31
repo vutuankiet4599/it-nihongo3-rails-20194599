5c143ab9c23bcf352b1b919f8595dda6e74b4c90
vutuankiet4599 <vutuankiet4599@gmail.com>
2023-01-31 12:34:21 +0700

    add detail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_10_081103) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_31_053007) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_10_081103) do
     t.integer "number_of_pages"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "detail"
   end
 
   create_table "users", force: :cascade do |t|
